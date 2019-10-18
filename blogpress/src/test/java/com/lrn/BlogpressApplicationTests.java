660aaeac6c8c9dcb64d5423940254b3b0f903c53
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-10-18 17:40:10 +0530

    Started with UI Layer

---
+++ blogpress/src/test/java/com/lrn/BlogpressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrn;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogpressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
