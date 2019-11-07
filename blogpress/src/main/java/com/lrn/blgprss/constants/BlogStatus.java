2afb56c70494e25240fc5a7894bc581c0fedb1a3
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-11-07 23:27:32 +0530

    Added this status' ENUMs

---
+++ blogpress/src/main/java/com/lrn/blgprss/constants/BlogStatus.java
@@ -0,0 +1,15 @@
+package com.lrn.blgprss.constants;
+
+public enum BlogStatus {
+	PUBLISHED("Published"), DRAFT("Draft");
+
+	private String status;
+
+	private BlogStatus(String status) {
+		this.status = status;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+}
