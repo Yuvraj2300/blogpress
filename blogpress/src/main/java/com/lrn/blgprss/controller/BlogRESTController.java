713009ae03f19886c0a841e855b2daccaefd1b46
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-11-19 18:03:49 +0530

    Created a REST API controller to get all the blogs as JSON response

---
+++ blogpress/src/main/java/com/lrn/blgprss/controller/BlogRESTController.java
@@ -0,0 +1,33 @@
+package com.lrn.blgprss.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lrn.blgprss.model.Blog;
+import com.lrn.blgprss.service.BlogService;
+
+@RestController
+@RequestMapping("/api")
+public class BlogRESTController {
+	private Logger logger = LoggerFactory.getLogger(BlogRESTController.class);
+
+	@Autowired
+	private BlogService blogService;
+	
+	@GetMapping(value="/listBlogs",produces=MediaType.APPLICATION_JSON_VALUE)
+	public	ResponseEntity<List<Blog>> getAllBlogJSON(){
+		logger.info("getting all the blogs in JSON format");
+		List<Blog> allBlogs	=	blogService.getAllBlogs();
+		//SKIPPING THE COMMENTS LOGIC FOR NOW
+		return new ResponseEntity<List<Blog>>(allBlogs,HttpStatus.OK);
+	}
+}
