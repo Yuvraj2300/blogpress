470e3e65cb1ee1692b727ee5c4ff0bd913e61a95
ys19299 <ys19299@DE-YS19299-01.zs.local>
2019-11-19 18:03:10 +0530

    Added a search query and also a logic to parse the response

--- blogpress/src/main/java/com/lrn/blgprss/repo/impl/BlogRepositoryCustomImpl.java
+++ blogpress/src/main/java/com/lrn/blgprss/repo/impl/BlogRepositoryCustomImpl.java
@@ -1,10 +1,22 @@
 package com.lrn.blgprss.repo.impl;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.jboss.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lrn.blgprss.model.Blog;
 import com.lrn.blgprss.repo.BlogRepositoryCustom;
 
 @Repository
@@ -13,4 +25,53 @@ public class BlogRepositoryCustomImpl implements BlogRepositoryCustom{
 	
 	@Autowired
 	private	ElasticsearchTemplate esTemp;
+	
+	
+	/**
+	 * @param searchText
+	 * @return Returns a list containing the blogs found by the query
+	 */
+	public List<Blog> search(String searchText){
+		
+		QueryBuilder booleanQry	=	QueryBuilders.boolQuery()
+				.should(QueryBuilders.termQuery("title", searchText))
+				.should(QueryBuilders.termQuery("body", searchText));
+		
+		SearchResponse response	= esTemp.getClient().prepareSearch("blog")
+				.setTypes("blog")
+				.setQuery(booleanQry)
+				.execute().actionGet();
+		
+		List<Blog> blogSearchResult	=	getBlogListFromSearchJSON(response.toString());
+		
+		return	null;
+	}
+
+
+	private List<Blog> getBlogListFromSearchJSON(String response) {
+		// TODO Auto-generated method stub
+		List<Blog> blogsList	=	new	 ArrayList<Blog>();
+		ObjectMapper	objMapper	=	new ObjectMapper();
+		if(response!=null) {
+			JSONObject	searchJson	=	new	JSONObject(response);
+			if(searchJson.get("hits")!=null) {
+				JSONObject resultJson	=	searchJson.getJSONObject("hits");
+				try {	
+					if(resultJson!=null || resultJson.get("hits")!=null) {
+						JSONArray blogArray = resultJson.getJSONArray("hits");
+
+						for(int i=0;i<blogArray.length();i++) {
+							blogsList.add(objMapper.readValue(blogArray.getJSONObject(i)
+									.getJSONObject("_source").toString()
+									, Blog.class));
+						}
+					}
+				}catch(JSONException | IOException e) {
+					logger.error("error occured while fetching all the comments"+ e.getMessage(),e);
+				}
+			}
+		}
+		return blogsList;
+	}
+
 }
